c38afc735e80cf864df15f8a309ac4aad83f941d
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2024-10-28 08:11:56 +0600

    Add files via upload

---
+++ Java basic code/div.java
@@ -0,0 +1,8 @@
+public class div {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 20;
+        int sum = a / b;
+        System.out.println("Sum of a and b is " + sum);
+    }
+}

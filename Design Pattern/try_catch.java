74d87f19d6d5b118052587a54641838d7d4c9781
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-01-28 21:53:10 +0600

    Update try_catch.java

--- Design Pattern/try_catch.java
+++ Design Pattern/try_catch.java
@@ -17,3 +17,15 @@ Simple Example :
         }
     }
 }
+
+
+What Happens Here:
+
+Inside the try block, 10 / 0 causes an error (division by zero).
+The catch block catches the error and prints: "You cannot divide a number by zero."
+The program doesn’t crash; it handles the error gracefully.
+  
+Why Use try-catch?
+  
+It stops the program from crashing when something unexpected happens.
+You can handle errors in a way that makes sense for your program.

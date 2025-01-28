ffdde98ce326970e962e39eb30f16d84febc70b1
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-01-28 21:44:21 +0600

    Update try_catch.java

--- Design Pattern/try_catch.java
+++ Design Pattern/try_catch.java
@@ -2,3 +2,12 @@
   executing instead of crashing. Here's the syntax and an example of how it works:*/
 
 //Syntex :
+try {
+    // Code that might throw an exception
+} catch (ExceptionType1 e1) {
+    // Code to handle exception of type ExceptionType1
+} catch (ExceptionType2 e2) {
+    // Code to handle exception of type ExceptionType2
+} finally {
+    // Optional: Code that will always execute, whether or not an exception was thrown
+}

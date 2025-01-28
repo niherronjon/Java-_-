0512dc8672e7c63004b92a0f2c842f604f2c5e17
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-01-28 21:52:07 +0600

    Update try_catch.java

--- Design Pattern/try_catch.java
+++ Design Pattern/try_catch.java
@@ -3,4 +3,17 @@ What is try-catch ?
 try: Put the code that might cause an error inside this block.
 catch: If an error happens, this block will handle it.
   
-Simple Example
+Simple Example : 
+
+  public class TryCatchExample {
+    public static void main(String[] args) {
+        try {
+            // Code that might cause an error
+            int number = 10 / 0; // This will cause an error (divide by zero)
+            System.out.println("Number: " + number);
+        } catch (ArithmeticException e) {
+            // Handle the error
+            System.out.println("You cannot divide a number by zero.");
+        }
+    }
+}

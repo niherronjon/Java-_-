48d92c663ebae224fe28671b207dd345407237ef
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-01-28 21:43:42 +0600

    Create try_catch.java

---
+++ Design Pattern/try_catch.java
@@ -0,0 +1,4 @@
+/*In Java, the try-catch block is used to handle exceptions, providing a way to deal with runtime errors so that the program can continue 
+  executing instead of crashing. Here's the syntax and an example of how it works:*/
+
+//Syntex :
